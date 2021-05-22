f2b8e828839de1452e64dcf523669c84774bfcf3
HermanQuaint <hermanquaintit@gmail.com>
2021-05-22 19:47:39 +0300

    home

--- src/components/Carusel.vue
+++ src/components/Carusel.vue
@@ -1,7 +1,7 @@
 <template>
-  <hooper :itemsToShow="3">
+  <hooper :settings="hooperSettings">
     <slide class="wrapper" v-for="(poster, indx) in getPosters" :key="indx" :index="indx">
-      {{ poster }}
+      <h2 class="title">{{ poster.title }}</h2>
       <img class="carusel-img" :src="img" alt="Афиша мероприятия">
     </slide>
   </hooper>
@@ -9,16 +9,30 @@
 </template>
 
 <script>
-import { Hooper, Slide } from 'hooper';
+import {Hooper, Slide} from 'hooper';
 import 'hooper/dist/hooper.css';
 import poster from "../store/poster";
 
 export default {
   data: () => ({
-    slide: '',
-    img: poster.img
+    img: poster.img,
+    hooperSettings: {
+      itemsToShow: 1.5,
+      breakpoints: {
+        500: {
+          itemsToShow: 2
+        },
+        800: {
+          itemsToShow: 3
+        },
+        1000: {
+          itemsToShow: 4
+        }
+      }
+    }
+
   }),
-methods:{},
+  methods: {},
 
   computed: {
     getPosters() {
@@ -30,14 +44,14 @@ methods:{},
 
     //getPosters() {
     //return this.$store.getters.getPosters
-  //},
+    //},
 
   },
-  async mounted () {
+  async mounted() {
     console.log(this.$store.state.posters)
   },
   created() {
-      this.$store.dispatch('poster/getAll')
+    this.$store.dispatch('poster/getAll')
   },
   name: 'Carusel',
   components: {
@@ -48,16 +62,42 @@ methods:{},
 </script>
 
 <style scoped>
+.carusel-img {
+  width: 100%;
+}
+
+.wrapper {
+  margin-right: 15px;
+
+  width: 170px;
+  height: 150px;
+
+  background-color: gray;
+  border-radius: 20px;
+}
+
+.title {
+  margin-left: 10px;
+  font-size: 1rem;
+
+}
+
+@media (min-width:800px) {
+  .title {
+    font-size: 1.3rem;
+  }
   .wrapper {
-    margin-right:15px;
-
-    width: 170px;
-    height: 110px;
-
-    background-color: gray;
-    border-radius: 20px;
+    height: 150px !important;
   }
-  .carusel-img{
-   width:100%;
+}
+@media (min-width:1000px) {
+  .title {
+    font-size: 1.3rem;
   }
+  .wrapper {
+    height: 200px;
+  }
+}
+
+
 </style>
